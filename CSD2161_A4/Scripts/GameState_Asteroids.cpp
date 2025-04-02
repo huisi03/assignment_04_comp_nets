9623aee6c84633c71412aba247717bd265e16862
nivethavarsni <nivethavarsni17@gmail.com>
2025-04-02 15:52:06 +0800

    Make the time look nicer

--- CSD2161_A4/Scripts/GameState_Asteroids.cpp
+++ CSD2161_A4/Scripts/GameState_Asteroids.cpp
@@ -180,7 +180,7 @@ std::string getCurrentTimeStamp() {
 	auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
 	std::ostringstream oss;
-	oss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%dT%H:%M:%S");
+	oss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M:%S");
 	return oss.str();
 }
 
